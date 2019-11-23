1db643ace74677a08ae0faf6f81126fb330dda68
Warren Yao <warrenyca905@gmail.com>
2019-11-23 18:56:26 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    binding.pry
     @post = Post.find(params[:id])
+
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
